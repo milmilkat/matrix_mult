2db83f3b2a1a2d69f43c49cc4b77581fe93f0d6c
Milad <mldktb@gmail.com>
2018-05-23 20:36:07 -0300

    sparse matrix multiplication

--- openmp/ommatrix.c
+++ openmp/ommatrix.c
@@ -14,12 +14,7 @@ Matrix_t matrix_mult_openmp(Matrix_t a, Matrix_t b, int number_threads, int chun
 	if(*(matrix_sparsity(a)+i) != -1)
 	  sparse_threads++;
 
-  
-  //for(size_t i = 0; i < a.rows; i++)
-	//printf("sth sth sth sth %d\n",*(matrix_sparsity(a)+i));
-
   nthreads = number_threads - sparse_threads;
-  printf("sth sth sth sth %d\n",nthreads);
 
   omp_set_dynamic(0);
   omp_set_num_threads(nthreads);
@@ -33,15 +28,15 @@ Matrix_t matrix_mult_openmp(Matrix_t a, Matrix_t b, int number_threads, int chun
 
     // Do matrix multiply sharing iterations on outer loop
     // Display who does which iterations for demonstration purposes
-    printf("Thread %d starting matrix multiply...\n",tid);
+    //printf("Thread %d starting matrix multiply...\n",tid);
     #pragma omp for schedule (static, chunk)
-    for (size_t i = 0; i < nthreads; i++)
+    for (size_t i = 0; i < nthreads+1; i++)
     {
 	  for(size_t m = 0; m < sparse_threads; m++)
-	    if(*(matrix_sparsity(a)+m) == i)
+	    if(*(matrix_sparsity(a)+m) == i && *(matrix_sparsity(a)) > -1)
 		  i++;
 		
-      printf("Thread=%d did row=%ld\n",tid,i);
+      //printf("Thread=%d did row=%ld\n",tid,i);
       for(size_t j = 0; j < b.cols; j++)
         for (size_t k = 0; k < a.cols; k++)
           result.items[i][j] += a.items[i][k] * b.items[k][j];
